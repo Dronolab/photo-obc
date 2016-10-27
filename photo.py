4a35d2317f3592a0775e438a68702a4c5db9db02
Pierre Bonzel <pierre.bonzel@gmail.com>
2016-10-26 22:27:31 -0400

    wip

--- photo.py
+++ photo.py
@@ -4,6 +4,7 @@ import pyexiv2
 import logging
 import sys
 import time
+import os
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
@@ -15,81 +16,139 @@ client = MongoClient('localhost', 27017)
 db = client['fly-database']
 collection = db['fly-collection']
 
-picStack = []
-count = 0
-
+"""
 class EventHandler(FileSystemEventHandler):
 	patterns = ["*.jpg"]
 	def process(self, event):
 		picStack[count] = event.src_path
-		count++
-	    print event.src_path, count
+		count = count + 1
 
 	def on_created(self, event):
 	    self.process(event)
-
+"""
 def dataFromTimestamp(timestamp):
-	dataset = json.loads('{"pitch": -1, "roll": -1, "yaw": -1, "latitude": -1, "longitude": -1, "heading": -1, "altitude": -1}')
-	datasets[]
+	dataset = json.loads('{"pitch": -1, "roll": -1, "yaw": -1, "latitude": -1, "longitude": -1, "heading": -1, "altitude": -1, "relative_alt": -1, "unix30": -1, "unix33": -1, "unixPicName": -1}')
 
 	res30 = list(collection.find({
 		"packet_id": 30,
-		"unix": { "$gte": float(float(timestamp) - 0.1)  },
-    	"unix": { "$lte": float(float(timestamp) + 0.1) }
+		"$and":[{
+				"unix": { "$gte":  float(float(timestamp) - 0.1), "$lte":  float(float(timestamp) + 0.1) }
+		    	}]
 		}))
 
 	res33 = list(collection.find({
 		"packet_id": 33,
-		"unix": { "$gte": float(float(timestamp) - 0.1) },
-    	"unix": { "$lte": float(float(timestamp) + 0.1) }
+		 "$and":[{
+				"unix": { "$gte":  float(float(timestamp) - 0.1), "$lte":  float(float(timestamp) + 0.1) }
+		    	}]
 		}))
 
-	ml = max(res30, res33)
+	bestRes30 = findBestMatch(res30, timestamp)
+	bestRes33 = findBestMatch(res33, timestamp)
 
-	for k in xrange(0,len(ml)):
-		for i in xrange(0,len(res30)):
-			dataset['yaw']=res30[i]['yaw']
-			dataset['pitch']=res30[i]['pitch']
-			dataset['roll']=res30[i]['roll']
-			datasets.insert(k, dataset)
+	dataset['yaw']=bestRes30['yaw']
+	dataset['pitch']=bestRes30['pitch']
+	dataset['roll']=bestRes30['roll']
+	dataset['latitude']=bestRes33['lat']
+	dataset['longitude']=bestRes33['lon']
+	dataset['altitude']=bestRes33['alt']
+	dataset['heading']=bestRes33['hdg']
+	dataset['relative_alt']=bestRes33['relative_alt']
+	dataset['unix30']=bestRes30['unix']
+	dataset['unix33']=bestRes33['unix']
+	dataset['unixPicName']=timestamp
 
-		for i in xrange(0,len(res33)):
-			dataset['latitude']=res33[i]['lat']
-			dataset['longitude']=res33[i]['lon']
-			dataset['altitude']=res33[i]['alt']
-			dataset['heading']=res33[i]['hdg']
-			datasets.insert(k, dataset)
-
-	return datasets
+	return dataset
 
 def findBestMatch(datasets, timestamp):
-	index = min(range(len(datasets)), key=lambda i: abs(datasets[i]['unix']-float(timestamp))
+	index = min(range(len(datasets)), key=lambda i: abs(datasets[i]['unix']-float(timestamp)))
+	print index
 	return datasets[index]
 
 def matchTimeFormat(timestamp):
-	return timestamp[:10] + '.' + timestamp[11:]
+	return timestamp[:10] + '.' + timestamp[10:] + '0'
 
-def stackPicName(path):
-	observer = Observer()
-    observer.schedule(EventHandler(), path, recursive=False)
-    observer.start()
+def to_deg(value, loc):
+	if value < 0:
+	   loc_value = loc[0]
+	elif value > 0:
+	   loc_value = loc[1]
+	else:
+	   loc_value = ""
+	abs_value = abs(value)
+	deg =  int(abs_value)
+	t1 = (abs_value-deg)*60
+	min = int(t1)
+	sec = round((t1 - min)* 60, 5)
+	return (deg, min, sec, loc_value)
 
+def set_gps_location(file_name, lat, lng, hdg, alt):
+
+	lat_deg = to_deg(lat, ["S", "N"])
+	lng_deg = to_deg(lng, ["W", "E"])
+
+	if alt < 0:
+	   	rel_byte = '1'
+	else:
+		rel_byte = '0'
+
+	print lat_deg
+	print lng_deg
+
+	# convert decimal coordinates into degrees, munutes and seconds
+	exiv_lat = (pyexiv2.Rational(lat_deg[0]*60+lat_deg[1],60),pyexiv2.Rational(lat_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+	exiv_lng = (pyexiv2.Rational(lng_deg[0]*60+lng_deg[1],60),pyexiv2.Rational(lng_deg[2]*100,6000), pyexiv2.Rational(0, 1))
+	exiv_hdg = pyexiv2.Rational(hdg, 360)
+	exiv_alt = pyexiv2.Rational(alt, 100000000)
+
+	print exiv_lat
+	print exiv_lng
+
+	exiv_image = pyexiv2.ImageMetadata(file_name)
+	exiv_image.read()
+	exif_keys = exiv_image.exif_keys
+
+	exiv_image["Exif.GPSInfo.GPSLatitude"] = exiv_lat
+	exiv_image["Exif.GPSInfo.GPSLatitudeRef"] = lat_deg[3]
+	exiv_image["Exif.GPSInfo.GPSLongitude"] = exiv_lng
+	exiv_image["Exif.GPSInfo.GPSLongitudeRef"] = lng_deg[3]
+	exiv_image["Exif.Image.GPSTag"] = 654
+	exiv_image["Exif.GPSInfo.GPSMapDatum"] = "WGS-84"
+	exiv_image["Exif.GPSInfo.GPSVersionID"] = '2 0 0 0'
+	exiv_image["Exif.GPSInfo.GPSDestBearing"] = exiv_hdg
+	exiv_image["Exif.GPSInfo.GPSDestBearingRef"] = 'M'
+	exiv_image["Exif.GPSInfo.GPSAltitude"] = exiv_alt
+	exiv_image["Exif.GPSInfo.GPSAltitudeRef"] = rel_byte
+
+	exiv_image.write()
+
+def picInception(picPath):
+	metadata = pyexiv2.ImageMetadata(picPath)
+	metadata.read()
+
+	print metadata.exif_keys
 
 def main():
-    path='/home/dev/photo/captures/'
+    path='/home/dronolab/dev/photo/captures/'
 
-    stackPicName(path)
-    tmp = 0
-	try:
-        while True:
-			picTime = matchTimeFormat(picStack[tmp])
-			d = findBestMatch(dataFromTimestamp(picTime), picTime)
-        	tmp++
-			print d
-    except KeyboardInterrupt:
-        observer.stop()
+    # picTime = matchTimeFormat("1477091472590")
+    # print picTime
+    # rawDatasets = dataFromTimestamp(picTime)
+    # print rawDatasets
+    fileList = dict ([(f, f.split('.')[0]) for f in os.listdir(path) if f.split('.')[1] == "jpg" if f[0] == '1'])
+    print fileList
 
-    observer.join()
+	# try:
+ #        while True:
+	# 		fileList = dict ([(f, f.split('.')[0]) for f in os.listdir (path) if f.split('.')[1] == "jpg"] and f.split('')[0] <= 9)
+
+	# 		picTime = matchTimeFormat("1477091472590")
+	# 	    print picTime
+	# 	    rawDatasets = dataFromTimestamp(picTime)
+	# 	    print rawDatasets
+	# 	    set_gps_location(path, float(rawDatasets['latitude']), float(rawDatasets['longitude']), float(rawDatasets['heading']), float(rawDatasets['altitude']))
+
+ #    except KeyboardInterrupt:
 
 if __name__ == "__main__":
     main()
